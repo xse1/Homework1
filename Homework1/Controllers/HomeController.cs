a4526a543b499b593282398c96080f9e8a0e9c31
alali <alali@ABDULLAH>
2023-05-23 13:26:31 +0300

    Add project files.

---
+++ Homework1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _1.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult ReverseName(string name)
+    {
+        if (name == null)
+        {
+            ViewBag.Message = "Check Your Name";
+        }
+        else
+        {
+            string reversedName = "";
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reversedName += name[i];
+            }
+
+            ViewBag.Message = reversedName;
+        }
+
+        return View("Index");
+    }
+}
+
